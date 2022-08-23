cb9c7f5a4e1714a86d3304412bafa13c486c60ab
Mahdi Behoftadeh <mahdi.behoftadeh.1381@gmail.com>
2022-08-23 16:54:45 +0430

    Added credits to main file

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -1,3 +1,13 @@
+# From https://behoftadeh.ir Written by Mahdi Behoftadeh
+
+# This is a simple Tic Tac Toe game made by Python.
+# This game takes two player names and will continue to take a movement in range of 1-9 and displays each players movements
+# and when there's 3 numbers in a row the player will win the game.
+
+# Apache License
+# Version 2.0, January 2004
+# http://www.apache.org/licenses/
+
 name_player_1 = input('Enter player 1\'s name: ')
 name_player_2 = input('Enter player 2\'s name: ')
 
